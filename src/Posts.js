f85d46658dd6d90260031abf77a6c52fb1c4ac83
Trenisha Goslee <60710670+tgoslee@users.noreply.github.com>
2021-07-08 21:47:15 -0400

    Initial commit

---
+++ src/Posts.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+
+const Posts = () => {
+  useEffect(() => {
+    getPosts();
+  });
+
+  const [posts, setPosts] = React.useState();
+
+  const getPosts = () => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts`)
+      .then((response) => {
+        setPosts(response.data);
+        console.log(posts);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>
+          title: {post.title},<div> body: {post.body} </div>
+          <hr />
+          <br />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Posts;
